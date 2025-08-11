2ca52b6736fd756620aa3415c7203f915a505aed
Naveen <naveen.b@juspay.in>
2025-08-11 12:58:09 +0530

    dependencies created

---
+++ src/main/java/com/example/DevBook/DevBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DevBook/DevBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
